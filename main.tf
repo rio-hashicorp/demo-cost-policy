267bfa1fcedb3bc2db11d42edade6833f96652c3
rio-hashicorp <165767096+rio-hashicorp@users.noreply.github.com>
2024-04-30 08:39:25 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,31 @@ terraform {
 }
 
 provider "aws" { 
-  # Configuration options 
+  region = var.region
 }
-    
+
+resource "aws_instance" "ubuntu" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
   
